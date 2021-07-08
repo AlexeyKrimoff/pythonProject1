872ecf42b3939162179913596ddce2bc83c1f914
Alexey <akrimoff@gmail.com>
2021-07-08 23:32:57 +0300

    Домашка к 3 уроку

--- venv/Lesson3/task5.py
+++ venv/Lesson3/task5.py
@@ -1,9 +1,16 @@
 
-def my_func():
-    user_in = input('Enter digits thrue the spaces  - ')
-    for el in user_in:
-        rez:int = sum(el)
-        return
+def sum_num():
+    summa = 0
+    while True:
+        in_list = input('Enter digits thrue  spaces or "A" - ').split()
+        for num in in_list:
+           if num == 'a':
+               return summa
+           else:
+               try:
+                   summa += int(num)
+               except ValueError:
+                   print('Чтобы выйти из программы нажмите a - ')
+        print(f'Сумма чисел = {summa}')
 
 
-print(my_func())
