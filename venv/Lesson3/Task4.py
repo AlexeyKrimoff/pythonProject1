3571e0d4c0660caa5fd7c24a386005ff85c790e0
Alexey <akrimoff@gmail.com>
2021-07-08 18:13:50 +0300

    Домашка к 3 уроку

---
+++ venv/Lesson3/Task4.py
@@ -0,0 +1,15 @@
+
+
+#def my_func(x, y):
+#    rez = x ** y
+#    return rez
+#print(my_func(2, -4))
+
+#x: int = 10
+#y: int = 3
+
+def my_func(x, y):
+    for el in range(y + 1):
+        el =  x * x
+
+print(my_func(2, -4))
---
+++ venv/Lesson3/task5.py
@@ -0,0 +1,9 @@
+
+def my_func():
+    user_in = input('Enter digits thrue the spaces  - ')
+    for el in user_in:
+        rez:int = sum(el)
+        return
+
+
+print(my_func())
