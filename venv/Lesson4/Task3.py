aa95d4abd971998f381cd058839895fc92eff7f8
Alexey <akrimoff@gmail.com>
2021-07-11 15:39:20 +0300

    К сожалению не смог разобраться с заданиями 1 и 7

---
+++ venv/Lesson4/Task3.py
@@ -0,0 +1,12 @@
+
+items = []
+for el in range(20, 240):
+    if el % 20 == 0:
+        items.append(el)
+print(f'Rezult for 20  - ',(items))
+
+items = []
+for el in range(20, 240):
+    if el % 21 == 0:
+        items.append(el)
+print(f'Rezult for 21  - ',(items))
